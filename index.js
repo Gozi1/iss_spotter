816c7d0f9b4a4746b81a326873f86c37f14cbee8
Gozi1 <snwabiani@gmail.com>
2022-11-02 03:24:13 +0000

    removed all unnecessary comments index

--- index.js
+++ index.js
@@ -1,37 +1,8 @@
-// const { fetchMyIP,fetchCoordsByIP,fetchISSFlyOverTimes } = require('./iss');
-
-// // fetchMyIP((error, ip) => {
-// //   if (error) {
-// //     console.log("It didn't work!" , error);
-// //     return;
-// //   }
-
-// //   console.log('It worked! Returned IP:' , ip);
-// // });
-// // const myIp ='24.36.166.86';
-// // const fakeIp ='42'
-
-// // fetchCoordsByIP(fakeIp,(error, data) => {
-
-// //   if (error) {
-// //         console.log("It didn't work!" , error);
-// //         return;
-// //       }
-    
-// //   console.log('It worked! Returned Coords are:' , data);
-
-// //   });
-// const   coords = {
-//   latitude: -20,
-//   longitude: 20,
-// }
-// fetchISSFlyOverTimes(coords,(error, data) => {
-
-//   if (error) {
-//         console.log("It didn't work!" , error);
-//         return;
-//       }
-    
-//   console.log('It worked! Returned ISS fly over times are:' , data);
-
-//   });
+const { nextISSTimesForMyLocation } = require('./iss');
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  console.log(passTimes);
+});
--- iss.js
+++ iss.js
@@ -65,5 +65,18 @@ const fetchCoordsByIP = (ip,callback) =>{
   })
 };
 
+/**
+ * Orchestrates multiple API requests in order to determine the next 5 upcoming ISS fly overs for the user's current location.
+ * Input:
+ *   - A callback with an error or results. 
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The fly-over times as an array (null if error):
+ *     [ { risetime: <number>, duration: <number> }, ... ]
+ */ 
+ const nextISSTimesForMyLocation = function(callback) {
+  // empty for now
+}
 
-module.exports = { fetchMyIP,fetchCoordsByIP,fetchISSFlyOverTimes };
+
+module.exports = { fetchMyIP,fetchCoordsByIP,fetchISSFlyOverTimes,nextISSTimesForMyLocation };
