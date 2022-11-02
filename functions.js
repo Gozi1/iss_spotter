23fbac696a141f570f65676db9f680f259bc1e37
Gozi1 <snwabiani@gmail.com>
2022-11-02 19:09:08 +0000

    moduralize printpasstimes and required it in both index files

---
+++ functions.js
@@ -0,0 +1,10 @@
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+module.exports = {printPassTimes};
--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 const { nextISSTimesForMyLocation } = require('./iss');
 
-const printPassTimes = function(passTimes) {
-  for (const pass of passTimes) {
-    const datetime = new Date(0);
-    datetime.setUTCSeconds(pass.risetime);
-    const duration = pass.duration;
-    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
-  }
-};
+const {printPassTimes} = require('./functions')
 
 nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
